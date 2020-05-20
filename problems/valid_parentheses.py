c2ce751851987b2c0db4fcb581d2e2d256c1ea16
xu.li <xu.li@aispeech.com>
2020-05-20 17:03:27 +0800

    complete 2 questions

---
+++ problems/valid_parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pairs = {'(':')',
+                '{':'}', 
+                '[':']'}
+        _stack = list()
+        for i in s:
+            if i in pairs:
+               _stack.append(i)
+            else:
+               if _stack:
+                   if pairs[_stack.pop()] != i:
+                        return False
+               else:
+                   return False
+        return len(_stack) == 0
