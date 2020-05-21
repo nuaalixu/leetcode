5ad1b380d9e429acdd4ba7884f7c82439971173f
xu.li <xu.li@aispeech.com>
2020-05-21 20:20:21 +0800

    add 2 new solutions

---
+++ problems/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums:
+            slow = 0
+            for fast in range(1, len(nums)):
+                if nums[slow] != nums[fast]:
+                    slow += 1
+                    nums[slow] = nums[fast]
+            return slow + 1
+        else:
+            return 0
